a43ebb095f5654c8d7ff699207bfd72a45fdfbc4
danielghost20 <danielramosmartinez72@gmail.com>
2023-09-04 21:40:50 -0600

    add: new credential

--- src/components/UploadImages.tsx
+++ src/components/UploadImages.tsx
@@ -3,9 +3,10 @@
 import { CldUploadButton } from 'next-cloudinary';
 
 export default function UploadImages() {
+
     return (
         <div>
-            <CldUploadButton onUpload={() => window.location.reload()} uploadPreset='priv69sx' className='w-full max-w-xs px-5 py-2 m-auto border-2 rounded-md' />
+            <CldUploadButton uploadPreset='priv69sx' className='w-full max-w-xs px-5 py-2 m-auto border-2 rounded-md' />
         </div>
     )
 
